138a54541278f5a9f64d5c8a2264e15b541bd365
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-01-04 21:14:46 +0530

    Time: 0 ms (100.00%), Space: 35.6 MB (61.99%) - LeetHub

---
+++ 476-number-complement/476-number-complement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int n) {
+        int ans=0;
+        int count=1;
+        while(n>0){
+            ans+=count*(n%2==0?1:0);
+            count*=2;
+            n/=2;
+        }
+        return ans;
+    }
+}
