3b1cff1ca416a1be94033442654eecf20890c509
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-02-16 15:55:36 +0530

    Added solution - LeetHub

---
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.java
@@ -0,0 +1,29 @@
+// { Driver Code Starts
+import java.util.*;
+class Replace{
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t-->0){
+		int n=sc.nextInt();
+		Solution g=new Solution();
+		System.out.println(g.convertFive(n));
+	}
+}
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+class Solution{
+
+	public static String convertFive(int n){
+    //add code here
+    String s= String.valueOf(n);
+    for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='0'){
+            s=s.replace('0','5');
+        }
+    }
+    return s;
+    }
+}
