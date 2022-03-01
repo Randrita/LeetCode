a909b7e38a354f857e5d87d4815332f467c0b5c1
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-03-01 22:15:51 +0530

    Time: 20 ms (9.52%), Space: 47.8 MB (41.65%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr=new int[n+1];
+        for(int i=0;i<=n;i++){
+            int count=0;
+            int temp=i;
+            while(temp>0){
+            if(temp%2 !=0){
+                count +=1;
+            }
+                temp=temp/2;
+            }
+            arr[i]=count;
+
+        }
+        
+        return arr;
+    }
+}
