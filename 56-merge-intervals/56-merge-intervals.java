f2182071ca4e5d1918529ae438fdb4f4ecc5af5c
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-07-09 18:38:40 +0530

    Time: 16 ms (36.19%), Space: 54.2 MB (82.72%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -7,28 +7,23 @@ class Solution {
         //sorting the given interval array based on starting point
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         
-        //defining start and end point
-        int start=intervals[0][0];
-        int end=intervals[0][1];
+       for(int[] interval:intervals){
+           if(list.size()==0){
+               list.add(interval);
+           }
+           
+           else{
+               
+               int[] prevInterval=list.get(list.size()-1);
+               if(interval[0]<=prevInterval[1]){
+                   prevInterval[1]=Math.max(prevInterval[1],interval[1]);
+               }else{
+                   list.add(interval);
+               }
+           }
+       }
         
-        //we will iterate through the 2d array intervals so in each iteration we will get a row[1D array] as i
-        
-        for(int[] i:intervals){
-            //check if end point of 1st pair if greater than the starting point of the 2nd pair or not, basically we check it's in overlapping condition or not
-            
-            if(i[0]<=end){
-                end=Math.max(end,i[1]);
-            }
-            else{
-                list.add(new int[]{start,end});
-                start=i[0];
-                end=i[1];
-            }
-            
-        }
-        
-        list.add(new int[]{start,end});
-        return list.toArray(new int[0][]);
+        return list.toArray(new int[list.size()][]);
         
     }
 }
